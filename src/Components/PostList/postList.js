5cecf2b619c035e459827a8d9347c5fd95b41bf6
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-07-06 17:30:21 -0400

    Feature: user can vote, downvote and remove vote

--- src/Components/PostList/postList.js
+++ src/Components/PostList/postList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer } from 'react';
-import labEdiit from '../../Services/labEdiit';
+import { loadPosts, votePost } from './postList_services';
 
 const initialState = [];
 
@@ -7,13 +7,15 @@ const postsReducer = (state, action) => {
     switch (action.type) {
         case 'LOAD_POSTS':
             return action.payload;
-        case 'VOTE_POST':
-            return;
-        case 'LOAD_POSTS':
-            return;
-        case 'LOAD_POSTS':
-            return;
-
+        case 'UPVOTE_POST':
+            votePost(action.id, 1);
+            return state;
+        case 'DOWNVOTE_POST':
+            votePost(action.id, -1);
+            return state;
+        case 'REMOVE_VOTE_FROM_POST':
+            votePost(action.id, 0);
+            return state;
         default:
             return state;
     }
@@ -23,23 +25,20 @@ export default function PostList() {
     const [posts, dispatch] = useReducer(postsReducer, initialState);
 
     useEffect(() => {
-        labEdiit
-            .get('/posts', {
-                headers: {
-                    Authorization:
-                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImlneXB1dFRXMkRWMDVMMTZIbVE0IiwidXNlcm5hbWUiOiI0ZWRkaXQxIiwiZW1haWwiOiI0ZWRkaXQxQGdtYWlsLmNvbSIsImlhdCI6MTU5NDA2MzkzNX0.2-CdXwv-cKRrX51lambuneYl4GhxqVrPF3WqrqLqOZg',
-                },
-            })
-            .then((response) => {
-                dispatch({ type: 'LOAD_POSTS', payload: response.data.posts });
-            });
+        fetchData();
     }, []);
+
+    async function fetchData() {
+        const postsPayload = await loadPosts();
+        dispatch({ type: 'LOAD_POSTS', payload: postsPayload });
+    }
 
     return (
         <div>
             {posts.map((post) => {
                 return (
                     <div key={post.id}>
+                        <p>{post.id}</p>
                         <p>{post.userVoteDirection}</p>
                         <p>{post.votesCount}</p>
                         <p>{post.createdAt}</p>
@@ -47,6 +46,46 @@ export default function PostList() {
                         <p>{post.commentsCount}</p>
                         <p>{post.title}</p>
                         <p>{post.username}</p>
+                        <button
+                            onClick={() =>
+                                dispatch({ type: 'UPVOTE_POST', id: post.id })
+                            }
+                        >
+                            {post.userVoteDirection === 1 ? (
+                                <p
+                                    onClick={() =>
+                                        dispatch({
+                                            type: 'REMOVE_VOTE_FROM_POST',
+                                            id: post.id,
+                                        })
+                                    }
+                                >
+                                    UPVOTED
+                                </p>
+                            ) : (
+                                'Upvote'
+                            )}
+                        </button>
+                        <button
+                            onClick={() =>
+                                dispatch({ type: 'DOWNVOTE_POST', id: post.id })
+                            }
+                        >
+                            {post.userVoteDirection === -1 ? (
+                                <p
+                                    onClick={() =>
+                                        dispatch({
+                                            type: 'REMOVE_VOTE_FROM_POST',
+                                            id: post.id,
+                                        })
+                                    }
+                                >
+                                    DOWNVOTED
+                                </p>
+                            ) : (
+                                'Downvote'
+                            )}
+                        </button>
                     </div>
                 );
             })}
--- src/Components/PostList/postList_services.js
+++ src/Components/PostList/postList_services.js
@@ -1,9 +1,32 @@
 import labEdiit from '../../Services/labEdiit';
 
-const votePost = () => {
-    labEdiit.put(`${id}/vote`, {
-        headers: {
-            Authorization: localStorage.getItem('userToken'),
-        },
-    });
+export const loadPosts = async () => {
+    try {
+        const response = await labEdiit.get('/posts', {
+            headers: {
+                Authorization:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImlneXB1dFRXMkRWMDVMMTZIbVE0IiwidXNlcm5hbWUiOiI0ZWRkaXQxIiwiZW1haWwiOiI0ZWRkaXQxQGdtYWlsLmNvbSIsImlhdCI6MTU5NDA2MzkzNX0.2-CdXwv-cKRrX51lambuneYl4GhxqVrPF3WqrqLqOZg',
+            },
+        });
+        return response.data.posts;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const votePost = async (id, direction) => {
+    try {
+        await labEdiit.put(
+            `posts/${id}/vote`,
+            { direction: direction },
+            {
+                headers: {
+                    Authorization:
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImlneXB1dFRXMkRWMDVMMTZIbVE0IiwidXNlcm5hbWUiOiI0ZWRkaXQxIiwiZW1haWwiOiI0ZWRkaXQxQGdtYWlsLmNvbSIsImlhdCI6MTU5NDA2MzkzNX0.2-CdXwv-cKRrX51lambuneYl4GhxqVrPF3WqrqLqOZg',
+                },
+            }
+        );
+    } catch (error) {
+        console.error(error);
+    }
 };
