c296e9530f52546a37300822334a1a28342e5f99
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-07-07 15:33:23 -0400

    (global): add token retrieve from localstorage

--- src/Components/PostList/postList_services.js
+++ src/Components/PostList/postList_services.js
@@ -4,8 +4,7 @@ export const loadPosts = async () => {
     try {
         const response = await labEdiit.get('/posts', {
             headers: {
-                Authorization:
-                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImlneXB1dFRXMkRWMDVMMTZIbVE0IiwidXNlcm5hbWUiOiI0ZWRkaXQxIiwiZW1haWwiOiI0ZWRkaXQxQGdtYWlsLmNvbSIsImlhdCI6MTU5NDA2MzkzNX0.2-CdXwv-cKRrX51lambuneYl4GhxqVrPF3WqrqLqOZg',
+                Authorization: localStorage.getItem('token'),
             },
         });
         return response.data.posts;
@@ -21,8 +20,7 @@ export const votePost = async (id, direction) => {
             { direction: direction },
             {
                 headers: {
-                    Authorization:
-                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImlneXB1dFRXMkRWMDVMMTZIbVE0IiwidXNlcm5hbWUiOiI0ZWRkaXQxIiwiZW1haWwiOiI0ZWRkaXQxQGdtYWlsLmNvbSIsImlhdCI6MTU5NDA2MzkzNX0.2-CdXwv-cKRrX51lambuneYl4GhxqVrPF3WqrqLqOZg',
+                    Authorization: localStorage.getItem('token'),
                 },
             }
         );
