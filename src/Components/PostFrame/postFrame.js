450c7478cb4b45ff03a05ba7fa7f9595033017fc
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-07-07 13:05:55 -0400

    feat(postlist): posts now have a link to details
    this feature is temporary

--- src/Components/PostFrame/postFrame.js
+++ src/Components/PostFrame/postFrame.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { Link } from 'react-router-dom';
 
 export default function PostFrame({
     id,
@@ -14,51 +15,57 @@ export default function PostFrame({
     dispatch,
 }) {
     return (
-        <div>
-            <p>{username}</p>
-            <p>{title}</p>
-            <p>{text}</p>
-            <p>{votesCount}</p>
-            <p>
-                {formatDistanceToNow(createdAt, {
-                    addSuffix: true,
-                    includeSeconds: true,
-                    locale: ptBR,
-                })}
-            </p>
-            <p>{commentsCount} 🗨</p>
-            <button onClick={() => dispatch({ type: 'UPVOTE_POST', id: id })}>
-                {userVoteDirection === 1 ? (
-                    <p
-                        onClick={() =>
-                            dispatch({
-                                type: 'REMOVE_VOTE_FROM_POST',
-                                id: id,
-                            })
-                        }
-                    >
-                        UPVOTED
-                    </p>
-                ) : (
-                    'Upvote'
-                )}
-            </button>
-            <button onClick={() => dispatch({ type: 'DOWNVOTE_POST', id: id })}>
-                {userVoteDirection === -1 ? (
-                    <p
-                        onClick={() =>
-                            dispatch({
-                                type: 'REMOVE_VOTE_FROM_POST',
-                                id: id,
-                            })
-                        }
-                    >
-                        DOWNVOTED
-                    </p>
-                ) : (
-                    'Downvote'
-                )}
-            </button>
-        </div>
+        <Link to={`/posts/${id}`}>
+            <div>
+                <p>{username}</p>
+                <p>{title}</p>
+                <p>{text}</p>
+                <p>{votesCount}</p>
+                <p>
+                    {formatDistanceToNow(createdAt, {
+                        addSuffix: true,
+                        includeSeconds: true,
+                        locale: ptBR,
+                    })}
+                </p>
+                <p>{commentsCount} 🗨</p>
+                <button
+                    onClick={() => dispatch({ type: 'UPVOTE_POST', id: id })}
+                >
+                    {userVoteDirection === 1 ? (
+                        <p
+                            onClick={() =>
+                                dispatch({
+                                    type: 'REMOVE_VOTE_FROM_POST',
+                                    id: id,
+                                })
+                            }
+                        >
+                            UPVOTED
+                        </p>
+                    ) : (
+                        'Upvote'
+                    )}
+                </button>
+                <button
+                    onClick={() => dispatch({ type: 'DOWNVOTE_POST', id: id })}
+                >
+                    {userVoteDirection === -1 ? (
+                        <p
+                            onClick={() =>
+                                dispatch({
+                                    type: 'REMOVE_VOTE_FROM_POST',
+                                    id: id,
+                                })
+                            }
+                        >
+                            DOWNVOTED
+                        </p>
+                    ) : (
+                        'Downvote'
+                    )}
+                </button>
+            </div>
+        </Link>
     );
 }
--- src/Pages/Feed/feed.js
+++ src/Pages/Feed/feed.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
+import PostList from '../../Components/PostList/postList';
 
 export default function Feed() {
-  return (
-    <div>
-      <h1>Feed</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Feed</h1>
+            <PostList />
+        </div>
+    );
 }
