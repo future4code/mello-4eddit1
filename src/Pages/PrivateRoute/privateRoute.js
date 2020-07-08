9757f3ef10b9946648fb35cfeac5fb0cdeebc8f1
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-07-08 18:07:53 -0400

    refactor(privateroutes): token match  does not call api anymore

--- src/Pages/PrivateRoute/privateRoute.js
+++ src/Pages/PrivateRoute/privateRoute.js
@@ -1,32 +1,15 @@
-import React, { useEffect } from 'react';
-import { Route, Redirect, useHistory } from 'react-router-dom';
-import { checkAuth } from './privateRoute_services';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
+const runClientAuthCheck = () => {
+    const token = localStorage.getItem('token');
+
+    return token != null &&
+        token.match(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.?[A-Za-z0-9-_.-_]*$/)
+        ? true
+        : false;
+};
 export function PrivateRoute({ ...props }) {
-    const history = useHistory();
-
-    useEffect(() => {
-        setTimeout(async () => {
-            await checkRoutePermission();
-        }, 2000);
-    }, [history]);
-
-    const runClientAuthCheck = () => {
-        const token = localStorage.getItem('token');
-        return String(token).length > 10 && token != null ? true : false;
-    };
-
-    const checkRoutePermission = async () => {
-        const check = await checkAuth();
-        console.log(check);
-        if (check === false) {
-            setTimeout(() => {
-                localStorage.clear();
-                history.push('/ogin');
-            }, 1000);
-        }
-    };
-
     return runClientAuthCheck() ? (
         <Route {...props} />
     ) : (
@@ -34,30 +17,7 @@ export function PrivateRoute({ ...props }) {
     );
 }
 
-/////////////////////////////////////
-
 export function LoginRoute({ ...props }) {
-    const history = useHistory();
-
-    const runClientAuthCheck = () => {
-        const token = localStorage.getItem('token');
-        return String(token).length > 10 && token != null ? true : false;
-    };
-
-    useEffect(() => {
-        setTimeout(async () => {
-            await checkRoutePermission();
-        }, 2000);
-    }, [history]);
-
-    const checkRoutePermission = async () => {
-        const check = await checkAuth();
-        console.log(check);
-        if (check === false) {
-            localStorage.clear();
-        }
-    };
-
     return runClientAuthCheck() ? (
         <Redirect to="/feed" />
     ) : (
